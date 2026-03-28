80f30ff9757e5c1226332c65fe737bcff903db98
snbaay <samatovice@gmail.com>
2026-03-28 17:10:02 +0500

    kuy response change

--- src/main/java/org/example/dombyra/dto/response/KuyResponse.java
+++ src/main/java/org/example/dombyra/dto/response/KuyResponse.java
@@ -1,4 +1,4 @@
 package org.example.dombyra.dto.response;
 
-public record KuyResponse(Long id, String name, String history, String audioUrl, KuyshyShortResponse kuyshy) {
+public record KuyResponse(Long id, String kuy_name, String history, String audioUrl, KuyshyShortResponse kuyshy) {
 }
