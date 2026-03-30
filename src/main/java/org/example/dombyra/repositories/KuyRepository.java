f612f01244bad1b37ead07dc8df20f6e322009f5
snbaay <samatovice@gmail.com>
2026-03-30 18:35:46 +0500

    kuy service change

--- src/main/java/org/example/dombyra/repositories/KuyRepository.java
+++ src/main/java/org/example/dombyra/repositories/KuyRepository.java
@@ -9,6 +9,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface KuyRepository extends JpaRepository<Kuy,Long> {
-    List<KuyResponse> findAllBy();
-    Optional<KuyResponse> findProjectedById(Long id);
+
 }
--- src/main/java/org/example/dombyra/services/KuyService.java
+++ src/main/java/org/example/dombyra/services/KuyService.java
@@ -3,12 +3,15 @@ package org.example.dombyra.services;
 import lombok.RequiredArgsConstructor;
 import org.example.dombyra.dto.response.KuyResponse;
 import org.example.dombyra.dto.response.KuyshyResponse;
+import org.example.dombyra.dto.response.KuyshyShortResponse;
+import org.example.dombyra.models.Kuy;
 import org.example.dombyra.repositories.KuyRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -16,9 +19,31 @@ public class KuyService {
     private final KuyRepository kuyRepository;
 
     public List<KuyResponse> getAllKuy(){
-        return kuyRepository.findAllBy();
+        List<Kuy> kuyler = kuyRepository.findAll();
+        return kuyler.stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
     }
+
     public KuyResponse getKuy(Long id){
-        return kuyRepository.findProjectedById(id).orElseThrow(() ->  new ResponseStatusException(HttpStatus.NOT_FOUND,"Kuy not found"));
+        Kuy kuy =  kuyRepository.findById(id).orElseThrow(() ->  new ResponseStatusException(HttpStatus.NOT_FOUND,"Kuy not found"));
+        return convertToDto(kuy);
+    }
+    private KuyResponse convertToDto(Kuy kuy) {
+        // Алдымен күйшінің қысқа нұсқасын жасаймыз
+        KuyshyShortResponse kuyshyShort = new KuyshyShortResponse(
+                kuy.getKuyshy().getId(),
+                kuy.getKuyshy().getFirstName(),
+                kuy.getKuyshy().getLastName()
+        );
+
+        // Сосын барып негізгі күйдің респонсын жинаймыз
+        return new KuyResponse(
+                kuy.getId(),
+                kuy.getName(), // (Егер сенде kuyName болса, солай жаз)
+                kuy.getHistory(),
+                kuy.getAudioUrl(),
+                kuyshyShort // Кішкентай күйшіні осында саламыз
+        );
     }
 }
